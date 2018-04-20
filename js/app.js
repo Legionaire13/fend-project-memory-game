29a51d02acb3f4a4d0bde893f078fc7eee89fc1f
legionaire13 <legionaire13@mail.ru>
2018-04-21 01:35:42 +0300

    feat: counter added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
-const moveCounter = 0; 
+let moveCounter = 0;
+const movesPlaceholder = document.querySelector('.moves');
+
+let cardPaired = 0;
 const timerInitialValue = 0;
 const openCardsList = [];
 
@@ -7,6 +10,8 @@ const openCardsList = [];
  * (+) Create a list that holds all of your cards
  */
 const startingDeck = document.querySelector('.deck');
+
+//stargame event listener
 startingDeck.addEventListener('click', readyToPlay);
 
 /*
@@ -29,7 +34,6 @@ function readyToPlay() {
 
     };
     clearDeck();
-    const openCardsList = []; //should be empty
 
     // Shuffle function from http://stackoverflow.com/a/2450976
     function shuffle(array) {
@@ -86,16 +90,11 @@ function readyToPlay() {
  *  - (+) display the card's symbol (put this functionality in another function that you call from this one)
  *  - (+) add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - (+) if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + (+) if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + (+) if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + (+) increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
-
-//добавить сюда потом все
-function gameSessionStart() {
-
-}
 
 //check if cards match
 function matchCheck(event) {
@@ -123,16 +122,62 @@ function matchCheck(event) {
             if (a.firstChild.classList[1] === b.firstChild.classList[1]) {
                 a.classList.add('match');//match visualization
                 b.classList.add('match');//match visualization
+                cardPaired++;
             }
+
             while(openCardsList.length) {
                 openCardsList.pop();//remove elements from "list"
             }
+            
             setTimeout(function() {                    
                 a.classList.remove('open', 'show');
                 b.classList.remove('open', 'show');
-            }, 500);
+            }, 400);
+
+            function setCounter() {
+                moveCounter++;
+                movesPlaceholder.textContent = moveCounter;
+            }
+            setCounter();
+            
+            //console.log(moveCounter);
         }
     }
+    
     isMatched();
+
+    function setStarRating() {
+        let starsContainer = document.querySelector('.stars');
+        let expr = 0;
+        switch (expr) {
+            case moveCounter > 20: 
+            1;
+              break;
+            case moveCounter > 14: 
+            2;
+              break;
+            default: 
+            3;
+        }
+        console.log(expr);
+
+        //переделать
+
+        // const fragment = document.createDocumentFragment();
+        // while (fragment.childElementCount < expr) {
+        //     const newStar = document.createElement('li');
+        //     const newStarItem = document.createElement('i');
+            
+        //     newStarItem.classList.add('fa fa-star');
+        //     newStar.append(newStarItem);
+        //     fragment.appendChild(newCard);
+        // }
+        // return starsContainer.append(fragment);
+    }
+    setStarRating();
+
+    function congratulationsShown() {
+        //console.log();
+    }
 
 }
